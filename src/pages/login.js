8ceeff56fd5aebaa5fc191cba70f61ceb605abe3
Iamvijay17 <vijay9578979255@gmail.com>
2024-06-20 23:25:20 +0530

    form init

--- src/pages/login.js
+++ src/pages/login.js
@@ -12,7 +12,7 @@ const Login = () => {
 
     const handleOnFinish = (values) => {
         toast.error("Api not Init");
-        navigate('/dashboard/create')
+        navigate('/home')
     };
     return (
         <div className={styles.loginWrapper}>
